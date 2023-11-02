2752919f1dc8f4c3be2c68f43fccf5b7b3d5fd64
Chetan VJ <98468274+ch374nvj@users.noreply.github.com>
2023-11-02 12:00:34 +0530

    Update sketch_nov2a.ino

--- sketch_nov2a/sketch_nov2a.ino
+++ sketch_nov2a/sketch_nov2a.ino
@@ -24,7 +24,8 @@ int left = 0;
 int right = 0;
 int front = 0;
 int pos = 0;
-char val;
+int dist = 0;
+int val;
 void setup() {
   pinMode(EN_A, OUTPUT);
   pinMode(EN_B, OUTPUT);
@@ -84,10 +85,39 @@ void stop() {
   digitalWrite (IN1, LOW);
   digitalWrite (IN2, LOW);
   digitalWrite (IN3, LOW);
-  digitalWrite (IN4, LOW);
+  digitalWrite (IN4, LOW);                                                                                                                                                                                                                                                                        
 }
 
 int sonarResponse() {
-  
-  return pos;
+    if (sonar.ping_cm()<=10 && sonar.ping_cm()!=0) 
+    {
+      myservo.write(LOOK_LEFT);
+      left = sonar.ping_cm();
+      delay(700);
+      myservo.write(LOOK_FRONT);
+      delay(100);
+      front = sonar.ping_cm();
+      delay(700);
+      myservo.write(LOOK_RIGHT);
+      delay(100);
+      right = sonar.ping_cm();
+      delay(700);
+      myservo.write(LOOK_FRONT);
+      
+      Serial.println("Left: ");
+      Serial.print(left);
+      Serial.println("Front: ");
+      Serial.print(front);
+      Serial.println("right: ");
+      Serial.print(right);
+    }
+    if (front >= 10)
+      return 0;
+    else if (left >= 10)
+      return 1;
+    else if (right >= 10)
+      return 2;
+    else
+      return 3;
+  // return val;
 }
